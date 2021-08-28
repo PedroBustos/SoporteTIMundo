d0da93b7696439cae10ae4b6e8ba9449722ea0e0
PedroBustos <88100153+PedroBustos@users.noreply.github.com>
2021-08-28 12:15:54 -0400

    Añadir LibreOffice 7.2.0
    se cambia versión de LibreOffice de 7.1.5 a 7.2.0

--- Scripts/instaladores.sh
+++ Scripts/instaladores.sh
@@ -30,19 +30,19 @@ sudo apt-get remove --purge libreoffice* -y
 sudo apt-get clean -y
 sudo apt-get autoremove -y
 
-#Para descargar el nuevo paquete de LibreOffice 7.0, vamos a ejecutar el siguiente comando:
+#Para descargar el nuevo paquete de LibreOffice 7.2.0, vamos a ejecutar el siguiente comando:
 
 
-wget http://download.documentfoundation.org/libreoffice/stable/7.1.5/deb/x86_64/LibreOffice_7.1.5_Linux_x86-64_deb.tar.gz
+wget http://download.documentfoundation.org/libreoffice/stable/7.2.0/deb/x86_64/LibreOffice_7.2.0_Linux_x86-64_deb.tar.gz
 
 #Hecha la descarga ahora podemos extraer el contenido del archivo descargado con:
 
 
-tar xvfz LibreOffice_7.1.5_Linux_x86-64_deb.tar.gz
+tar xvfz LibreOffice_7.2.0_Linux_x86-64_deb.tar.gz
 
 #Entramos al directorio creado:
 
-cd LibreOffice_7.1.5.2_Linux_x86-64_deb/DEBS/
+cd LibreOffice_7.2.0.2_Linux_x86-64_deb/DEBS/
 
 #Y finalmente instalamos los paquetes que están dentro de este directorio con el siguiente comando:
 
@@ -53,17 +53,18 @@ sudo dpkg -i *.deb
 
 cd ..
 cd ..
-wget http://download.documentfoundation.org/libreoffice/stable/7.1.5/deb/x86_64/LibreOffice_7.1.5_Linux_x86-64_deb_langpack_es.tar.gz
+wget http://download.documentfoundation.org/libreoffice/stable/7.2.0/deb/x86_64/LibreOffice_7.2.0_Linux_x86-64_deb_langpack_es.tar.gz
 
 #Y procedemos a descomprimir ya instalar los paquetes resultantes:
 
-tar xvfz LibreOffice_7.1.5_Linux_x86-64_deb_langpack_es.tar.gz
-cd LibreOffice_7.1.5.2_Linux_x86-64_deb_langpack_es/DEBS/
+tar xvfz LibreOffice_7.2.0_Linux_x86-64_deb_langpack_es.tar.gz
+cd LibreOffice_7.2.0.2_Linux_x86-64_deb_langpack_es/DEBS/
 sudo dpkg -i *.deb
 
 #Finalmente, en caso de tener algún problema con las dependencias, podemos ejecutar el siguiente comando:
 
 sudo apt-get -f install
+
 
 
 #instalacion VIM
